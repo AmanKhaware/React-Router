0e626795d3b1b0b7652b3e92415a2bcd27b00dd9
ESOLUTIONS\Aman.Khaware <Aman.Khaware@trivium-esolutions.com>
2023-05-29 10:35:48 +0530

    Add project files.

---
+++ react-router-nav/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import AfterLoginConfig from "./AfterLoginConfig";
+
+function App() {
+  return <AfterLoginConfig />;
+}
+
+export default App;
